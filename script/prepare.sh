6ae4390845d46c74012db3e6b5783b59548b006d
ivyxjc <ivyxjc1994@hotmail.com>
2023-04-08 23:35:26 +0800

    fix!

--- script/prepare.sh
+++ script/prepare.sh
@@ -31,7 +31,7 @@ if [ $OS == "ubuntu" ] || [ $OS == "arch" ]; then
     sudo apt-get update
     sudo locale-gen "en_US.UTF-8"
     # prepare for pyenv
-    sudo apt install build-essential libssl-dev zlib1g-dev \
+    sudo apt install -y build-essential libssl-dev zlib1g-dev \
         libbz2-dev libreadline-dev libsqlite3-dev curl \
         libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 fi
